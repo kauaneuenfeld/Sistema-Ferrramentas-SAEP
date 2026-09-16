4afc255206b4893516bbbd6e874f4ba95a0487ad
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-09-16 10:21:22 -0300

    ok

--- backend/app.js
+++ backend/app.js
@@ -52,6 +52,31 @@ server.get('/produtos/busca/:nome', (req, res) => {
     })
 })
 
+server.post('/produtos', (req, res) => {
+    const { nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario, id_categoria } = req.body;
+
+    if (nome == null || peso == null || unidade_medida == null || aplicacao == null || data_validade == null || estoque_minimo == null || estoque_atual == null || preco_unitario == null || id_categoria == null) {
+        return res.status(400).json({ erro: "Todos os campos devem ser preenchidos!" })
+    }
+
+    const sql = `
+        INSERT INTO PRODUTO
+            (nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario, id_categoria)
+        VALUES 
+            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    connection.query(sql, [nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario, id_categoria], (erro, resultados) => {
+        if (erro) {
+            return res.status(500).json({ erro: erro.message })
+        }
+        res.json({
+            message: "Produto cadastrado com sucesso!",
+            id: resultados.insertId
+        })
+    })
+})
+
 server.listen(8070, () => {
     console.log('Servidor rodando na porta 8070');
 });
